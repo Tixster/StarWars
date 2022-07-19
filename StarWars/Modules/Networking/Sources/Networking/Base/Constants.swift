e19c2e02604add3fbdabccbfa3023e0c08723d38
Тила Кирилл <tilakirill98@gmail.com>
2022-07-19 13:40:01 +0700

    Добавил модуль для сетевого слоя

---
+++ StarWars/Modules/Networking/Sources/Networking/Base/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by Кирилл Тила on 19.07.2022.
+//
+
+import Foundation
+
+public struct Constants {
+    public static let baseScheme: String = "https"
+    public static let baseHost: String = "swapi.dev"
+    public static let basePath: String = "/api"
+}
